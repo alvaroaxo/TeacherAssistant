9d98f8bfee1e5f55bd889a8fd5c3822fa6a36dc9
alvaroaxo <alvaroaxo78@gmail.com>
2018-03-15 21:28:44 -0400

    Add files via upload

---
+++ Conectar.cs
@@ -0,0 +1,20 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace newConecction
+{
+    class Conectar
+    {
+        public static MySqlConnection ObtenerConexcion()
+        {
+            MySqlConnection conectar = new MySqlConnection("server=192.168.0.16; database=liquidacion_sie; Uid=root; pwd=;");
+            conectar.Open();
+            return conectar;
+        }
+    }
+}
+
